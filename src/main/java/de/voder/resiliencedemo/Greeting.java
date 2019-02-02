1ab4e380a7f9c2103e6715f82002781d850cc857
Volker Aufschild <mail@volker-aufschild.de>
2019-02-02 15:17:57 +0100

    simple StartController

---
+++ src/main/java/de/voder/resiliencedemo/Greeting.java
@@ -0,0 +1,13 @@
+package de.voder.resiliencedemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+
+    private String greeting;
+}
