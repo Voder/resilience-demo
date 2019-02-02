aeb1d14df8703dd1cbfd0207afedcfba5346720a
Volker Aufschild <mail@volker-aufschild.de>
2019-02-02 16:47:01 +0100

    add retry combined with hystrix

--- src/main/java/de/voder/resiliencedemo/GreetingService.java
+++ src/main/java/de/voder/resiliencedemo/GreetingService.java
@@ -3,6 +3,7 @@ package de.voder.resiliencedemo;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,16 +12,19 @@ public class GreetingService {
 
     String url = "http://localhost:8081/hello";
     private RestTemplate restTemplate;
+    private RetryTemplate retryTemplate;
 
-    public GreetingService(RestTemplate restTemplate) {
+    public GreetingService(RestTemplate restTemplate, RetryTemplate retryTemplate) {
         this.restTemplate = restTemplate;
+        this.retryTemplate = retryTemplate;
     }
 
     @HystrixCommand(fallbackMethod = "greetingFallback", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "200")
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
     })
     public Greeting getGreeting() {
-        ResponseEntity<String> hello = restTemplate.getForEntity(url, String.class);
+
+        ResponseEntity<String> hello = retryTemplate.execute(arg -> restTemplate.getForEntity(url, String.class));
 
         return new Greeting(hello.getBody());
     }
--- src/main/java/de/voder/resiliencedemo/ResilienceDemoApplication.java
+++ src/main/java/de/voder/resiliencedemo/ResilienceDemoApplication.java
@@ -5,15 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableRetry
 public class ResilienceDemoApplication {
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
+	}
+
+	@Bean
+	public RetryTemplate retryTemplate() {
+		RetryTemplate retryTemplate = new RetryTemplate();
+
+		FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+		fixedBackOffPolicy.setBackOffPeriod(500l);
+		retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+		SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+		retryPolicy.setMaxAttempts(2);
+		retryTemplate.setRetryPolicy(retryPolicy);
+		retryTemplate.setThrowLastExceptionOnExhausted(true);
+
+		return retryTemplate;
 	}
 
 	public static void main(String[] args) {
