5b361a7eb29d468bc208217e5dad03f09d237a38
Volker Aufschild <mail@volker-aufschild.de>
2019-02-02 14:47:33 +0100

    inital start.spring.io project

---
+++ src/test/java/de/voder/resiliencedemo/ResilienceDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package de.voder.resiliencedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResilienceDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
